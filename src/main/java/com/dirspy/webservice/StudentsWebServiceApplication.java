933f6625502d13c909f4897b2b3c5aec614bcf4e
Victor Moskvych <Victor Moskvych>
2016-02-15 09:45:23 +0200

    Initial commit Add generated files by Spring Initializr for gradle project

---
+++ src/main/java/com/dirspy/webservice/StudentsWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.dirspy.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsWebServiceApplication.class, args);
+	}
+}
